550401f1e53dc375cce24f7920e3b69882319489
Nkululeko Sibeko <112828662+nkululekosibeko@users.noreply.github.com>
2024-09-28 05:31:09 +0200

    The Admin can now sign out.

--- app/src/main/java/com/example/durbanfirst/AdminActivity.java
+++ app/src/main/java/com/example/durbanfirst/AdminActivity.java
@@ -1,6 +1,9 @@
 package com.example.durbanfirst;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -10,10 +13,31 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class AdminActivity extends AppCompatActivity {
 
+    // Image Buttons
+    ImageView ViewUserBookings, ViewExistingUsers;
+
+    // Regular Button
+    Button AdminLogout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        // IMage Buttons
+        ViewUserBookings = findViewById(R.id.users_appointments);
+        ViewExistingUsers = findViewById(R.id.users_history);
+
+        // Buttons
+        AdminLogout = findViewById(R.id.admin_logout);
+
+
+        AdminLogout.setOnClickListener(v -> {
+            Intent intent = new Intent(AdminActivity.this, WelcomeActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
 
         // Enable edge-to-edge layout
         WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
